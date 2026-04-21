9d4f425a4d6840f997255a3d4ada52aed394ec48
gorgoroth31 <it.glen@mailbox.org>
2026-04-21 18:50:32 +0200

    functioning frontend-backend communication

--- sensoren.api/main.go
+++ sensoren.api/main.go
@@ -1,7 +1,38 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+
+	router.Use(cors.New(config))
+
+	router.GET("/data", getData)
+	err := router.Run(":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getData(c *gin.Context) {
+	var v data
+
+	v.Co2 = 1337
+	v.Luftfeuchtigkeit = 69.42
+
+	c.IndentedJSON(http.StatusOK, v)
+}
+
+type data struct {
+	Luftfeuchtigkeit float32 `json:"luftfeuchtigkeit"`
+	Co2              int     `json:"co2"`
 }
--- sensoren.client/script.js
+++ sensoren.client/script.js
@@ -1,10 +1,21 @@
-function loadData() {
-  // get values from api
-  console.log("fetching data...")
-  let luftfeuchtigkeit = 100;
-  document.getElementById("luftfeuchtigkeit").innerText = luftfeuchtigkeit;
-  let co2Wert = 444;
-  document.getElementById("co2-wert").innerText = co2Wert;
+async function getData() {
+  const url = "http://127.0.0.1:8080/data";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    let luftfeuchtigkeit = result["luftfeuchtigkeit"];
+    document.getElementById("luftfeuchtigkeit").innerText = luftfeuchtigkeit;
+    let co2Wert = result["co2"];
+    document.getElementById("co2-wert").innerText = co2Wert;
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-loadData()
+getData()
